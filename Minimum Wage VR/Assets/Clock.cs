d3cbb212e64826fdc1746edacd0473ba6108034a
BartKort <bartkortaudio@gmail.com>
2025-04-05 18:08:22 +0200

    Clock script

--- Minimum Wage VR/Assets/Clock.cs
+++ Minimum Wage VR/Assets/Clock.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float TimeLeft;
+	public bool TimerOn = false;
+	
+	// Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +16,28 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(TimerOn)
+		{
+			if(TimeLeft > 0)
+			{
+				TimeLeft -= Time.deltaTime;
+			} else {
+				Debug.Log("Time is up!");
+				TimeLeft = 0;
+				TimerOn = false;
+			}
+		}
     }
+	
+	void ClockActive(){
+		TimerOn = true;
+	}
+	
+	void updateTimer(float currentTime)
+	{
+		currentTime += 1;
+		
+		float minutes = Mathf.FloorToInt(currentTime / 60);
+		float seconds = Mathf.FloorToInt(currentTime % 60);
+	}
 }
