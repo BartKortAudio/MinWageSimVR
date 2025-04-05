c12bcee8e00a7c49e9b6075494c3b38c9aa70a6e
DESKTOP-AFLQFFS\Gebruiker <arjentromp12@hotmail.com>
2025-04-05 17:55:59 +0200

    2de update voor mp test

---
+++ Minimum Wage VR/Assets/NetworkConnecter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+public class NetworkConnecter : MonoBehaviour
+{
+    public void Host()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void Join()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
