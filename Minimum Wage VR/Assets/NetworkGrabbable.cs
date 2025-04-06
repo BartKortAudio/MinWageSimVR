490fcbfb878f3846a5a0180a998bd270c39e7e9e
DESKTOP-AFLQFFS\Gebruiker <arjentromp12@hotmail.com>
2025-04-06 19:39:00 +0200

    debugging

--- Minimum Wage VR/Assets/NetworkGrabbable.cs
+++ Minimum Wage VR/Assets/NetworkGrabbable.cs
@@ -19,9 +19,11 @@ public class NetworkGrabbable : NetworkBehaviour
 
     private void OnSelectEntered(SelectEnterEventArgs args)
     {
+        Debug.Log($"[{NetworkManager.Singleton.LocalClientId}] Attempting to grab {gameObject.name}");
         // Only request ownership if we're a client (not the host) and not already the owner
         if (IsClient && !IsOwner)
         {
+            Debug.Log("Requesting ownership from server...");
             RequestGrabServerRpc();
         }
     }
@@ -29,7 +31,10 @@ public class NetworkGrabbable : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void RequestGrabServerRpc(ServerRpcParams rpcParams = default)
     {
+
         ulong requestingClientId = rpcParams.Receive.SenderClientId;
+
+        Debug.Log($"Server received grab request from client {requestingClientId} for {gameObject.name}");
 
         // Transfer ownership to the grabbing client
         NetworkObject.ChangeOwnership(requestingClientId);
